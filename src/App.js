9af40366a5a546c68a93f9d78af3083187dd58cd
Pawar <kiranpawar@deloitte.com>
2023-06-01 12:53:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h2>React Course</h2>
+      <Todo text ='Learn React'/>
+      <Todo text ='Master React'/>
+      <Todo text ='Explore the full React course'/>
+    </div>
+  );
+}
+
+export default App;
